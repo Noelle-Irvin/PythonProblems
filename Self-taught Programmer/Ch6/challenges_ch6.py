6d843bb725cab43da78a0b79d81f413fa54f1ab9
Irvin <noelle.irvin@va1macy70cmjhd4.newcorp.com>
2019-06-13 17:17:04 -0400

    ch 5 complete

---
+++ Self-taught Programmer/Ch6/challenges_ch6.py
@@ -0,0 +1,7 @@
+# 1.
+string = "Camus"
+print(string[0])
+print(string[1])
+print(string[2])
+print(string[3])
+print(string[4])
---
+++ Self-taught Programmer/ch5/ch5_challenges.py
@@ -0,0 +1,23 @@
+# 1.
+listOfMusicians = ["Taylor Swift", "Carrie Underwood", "Sleeping At Last"]
+print(listOfMusicians)
+
+# 2.
+tupleOfLists = ([1,2],[3,4],[5,6])
+print(tupleOfLists)
+
+# 3.
+dictionaryAboutMe = {"height":"5 foot 7 inches", "eye color":"green"}
+print(dictionaryAboutMe)
+
+# 4.
+question = input("What attribute do you want to know about me? (height or eye color):")
+print(dictionaryAboutMe[question])
+
+# 5.
+music = {"taylor swift" : ["ME", "Love Story", "Red"] }
+print(music)
+
+# 6.
+# A Set is an unordered collection data type that is iterable, mutable
+# and has no duplicate elements.
